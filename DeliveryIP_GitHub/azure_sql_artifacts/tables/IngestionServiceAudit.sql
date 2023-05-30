57567c1c89cd7a2fc34752419a05b631fa4ac033
Rachael Phillips <rachael.aphillips@outlook.com>
2023-05-30 16:37:47 -0700

    Deleted things

--- DeliveryIP_GitHub/azure_sql_artifacts/tables/IngestionServiceAudit.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE dbo.IngestionServiceAudit (
-    Id INT IDENTITY(1,1)
-    ,LogDateTime DATETIME DEFAULT GETDATE()
-    ,HttpRequest NVARCHAR(MAX)
-    ,AuditMessage NVARCHAR(255)
-    ,MetaDataInserted BIT
-);
-GO
-
